b21bd6b68324060285bbc9dc904d44a7090c6dac
annalyssastakes <astakes@hotmail.com>
2023-06-08 16:56:36 -0500

    Adding background, removing Privacy from top nav bar

---
+++ cornapedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace cornapedia.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
